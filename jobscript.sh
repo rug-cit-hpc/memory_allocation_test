ad779800a5ad09e4803db0876addb091bc570773
Fokke Dijkstra <f.dijkstra@rug.nl>
2021-06-28 08:52:27 +0200

    Improved jobscript.

--- jobscript.sh
+++ jobscript.sh
@@ -1,12 +1,46 @@
 #!/bin/bash
-#SBATCH --nodes=1
-#SBATCH --mem-per-cpu=2G
-#SBATCH --tasks-per-node=4
+#SBATCH --nodes=2
+#SBATCH --tasks-per-node=2
 #SBATCH --cpus-per-task=2
+#SBATCH --mem-per-cpu=3G
 #SBATCH --time=00:10:00
-#SBATCH --partition=vulture
 
-echo ### Jobscript ###
-cat jobscript.sh
+module purge
+module load foss/2020a
+
+if [ "x$1" == "xsrun" ]; then
+   jobtype=srun
+elif [ "x$1" == "xmpirun" ]; then
+   jobtype=mpirun
+else
+   jobtype=nompi
+fi   
+
+if [ "x$2" == "xunbalanced" ]; then
+   allocation=unbalanced
+   flag="-u"
+else 
+   allocation=balanced
+   flag=""
+fi
+
+echo "Job Id                   :" $SLURM_JOB_ID
+echo "Job Type                 :" $jobtype
+echo "Number of nodes          :" $SLURM_JOB_NUM_NODES
+echo "Number of tasks per node :" $SLURM_TASKS_PER_NODE
+echo "CPUs per task            :" $SLURM_CPUS_PER_TASK
+echo "Memory allocation        :" $allocation
+
+export NUM_THREADS=${SLURM_CPUS_PER_TASK}
+
 echo ### Job output ###
-srun ./mpi_alloc.x -s 1024 -n ${SLURM_CPUS_PER_TASK} -t 300
+if [ "x$jobtype" == "xsrun" ]; then
+   echo "Invoking srun"
+   srun ./mpi_alloc.x -s 1024 -n ${NUM_THREADS} -t 300 $flag
+elif [ "x$jobtype" == "xmpirun" ]; then
+   echo "Invoking mpirun"
+   mpirun ./mpi_alloc.x -s 1024 -n ${NUM_THREADS} -t 300 $flag
+else 
+   echo "Invoking program directly"
+   ./mpi_alloc.x -s 1024 -n ${NUM_THREADS} -t 300 $flag
+fi
